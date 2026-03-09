242aeb6a5f85da3edad80017e8138fc727867b72
Bartłomiej Kulecki <37365394+BartlomiejKulecki@users.noreply.github.com>
2026-03-09 11:57:50 +0100

    Check for pilz_ros2 directory before cloning

--- src/setup_ws.sh
+++ src/setup_ws.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 cd ~/ros2_ws
-git clone -b jazzy https://github.com/LRMPUT/pilz_ros2.git src/pilz_ros2
+test -d src/pilz_ros2 || git clone -b jazzy https://github.com/LRMPUT/pilz_ros2.git src/pilz_ros2
 source /opt/ros/jazzy/setup.bash
 rosdep update --rosdistro jazzy
 rosdep install --from-paths src --ignore-src -y
