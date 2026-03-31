f72c265c915a8d64cadc4936297fede13d233f91
Bartłomiej Kulecki <37365394+BartlomiejKulecki@users.noreply.github.com>
2026-03-31 10:58:53 +0200

    Modify setup_ws.sh for pilz_ros2 repository management

--- src/setup_ws.sh
+++ src/setup_ws.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 sudo apt-get update
+cd ~/ros2_ws/src
+
+test -d pilz_ros2 || git clone -b jazzy https://github.com/LRMPUT/pilz_ros2.git
+cd pilz_ros2 && git pull origin jazzy
+
 cd ~/ros2_ws
-test -d src/pilz_ros2 || git clone -b jazzy https://github.com/LRMPUT/pilz_ros2.git src/pilz_ros2
 source /opt/ros/jazzy/setup.bash
 rosdep update --rosdistro jazzy
 rosdep install --from-paths src --ignore-src -y
 
-# colcon build --symlink-install
+colcon build --symlink-install
